26cbadbf6b58d776fc3bea0de2a88f701345aaf2
esthervankleef <esther.van.kleef@gmail.com>
2025-09-30 12:18:23 +0200

    Delete unused scripts

--- Scripts/Summarise_model_output/Summarise_simulated_data.R
+++
@@ -1,9 +0,0 @@
-####################################################################
-# SUMMARISE SIMULATED DATA OUTPUT
-####################################################################
-
-rm(list=ls())
-
-os = readRDS("./Output/Model_results/Validation_with_simulated_data/Two_step_sine_seasonality.rds")
-ons = readRDS("./Output/Model_results/Validation_with_simulated_data/Two_step_sine_noseasonality.rds")
-
